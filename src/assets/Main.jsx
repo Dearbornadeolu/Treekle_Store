ebccf6fa3a2c79142323538163ff707b22a56050
Dearborn Adeyeni <dearbornadeyeni@gmail.com>
2023-08-30 23:24:04 +0100

    data fetched

--- src/assets/Main.jsx
+++ src/assets/Main.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
-import "../App.css"
+import React, { useState, useEffect } from 'react';
 
-const Main = () => {
-  return (
-    <div className='main'>Main</div>
-  )
+function YourComponent() {
+    const [productsData, setProductsData] = useState({
+         products: [
+        { id: 1, title: 'iPhone 9', description: 'An apple mobile which is nothing like apple', price: 549, discountPercentage: 12.96 },
+      ],
+      total: 0,
+      skip: 0,
+      limit: 0
+});
+
+    useEffect(() => {
+        async function fetchProducts() {
+            try {
+                const response = await fetch('https://dummyjson.com/products');
+                const data = await response.json();
+                setProductsData(data); 
+            } catch (error) {
+                console.error('Error fetching products:', error);
+            }
+        }
+
+        fetchProducts();
+    }, []);
+
+
+    console.log(productsData)
+
+    return (
+        <div>
+            <h1>Products</h1>
+            <ul>
+                {productsData.products.map(product => (
+                    <li key={product.id}>
+                        {product.title}
+                        <br />
+                        {product.description} <br />
+                        {product.price} <br />
+                        {product.discountPercentage}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
-export default Main
+export default YourComponent;
