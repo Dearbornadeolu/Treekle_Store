efd90a5a9c358ce95ce94943d81185a7af247c98
Bossogo <ogooluwailori@gmail.com>
2023-09-09 16:57:52 +0100

    fixed deployment issue

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import { ProductProvider } from './comp/ProductContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-    <ProductProvider>
     <App />
-    </ProductProvider>
     </BrowserRouter>
 
   </React.StrictMode>,
